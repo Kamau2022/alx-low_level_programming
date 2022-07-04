5fe517c3d4983bd5f6b94fa9b7c521687e6ccc62
Benson <benkabenson@gmail.com>
2022-07-04 15:34:54 -0700

    Printing hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Description:'printing hexadecimal numbers in lower case'
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int i;
+char c;
+
+for (i = 0; i <= 9; ++i)
+putchar(i + '0');
+
+for (c = 'a'; c <= 'f'; ++c)
+putchar(c);
+
+return (0);
+}

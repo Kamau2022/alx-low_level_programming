22e3cbf802bd763b2153c0686b4c33968b362b32
Benson <benkabenson@gmail.com>
2022-07-06 14:34:46 -0700

    Add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*add(int,int) - Entry point
+*@int: An input integer
+*Description: The function adds two integers and returns result
+Return: result
+*/
+
+int add(inta, intb)
+{
+int a;
+int b;
+int result;
+result = a + b;
+return (result);
+}
+
+

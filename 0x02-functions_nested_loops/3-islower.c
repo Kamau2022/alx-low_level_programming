b35b97d6e334d058e968e915cee054b05139e4d0
Benson <benkabenson@gmail.com>
2022-07-06 11:55:27 -0700

    Check whether the alphabet is lower case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
-*int _islower 'Checks whether an alphabet is lower case'
-*Return: 1 if it is lower case or o if it is upper case
+*_islower -> This function checks whether an alphabet is lower case
+*@c: An input character
+*Description: checks whether alphabet is lower case
+*Return: 1 if is lower case or 0 if it is uppercase
 */
 
 int _islower(int c)
 {
 char h;
-int lower =0;
+int lower = 0;
 for (h = 'a'; h <= 'z'; ++h)
 {
 if (h == c)

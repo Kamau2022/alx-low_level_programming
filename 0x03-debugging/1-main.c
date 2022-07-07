bd76a609938d466cca3a9f6d1bd78d1f12c39c14
Benson <benkabenson@gmail.com>
2022-07-07 05:59:06 -0700

    debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - causes an infinite loop
+*Return: 0
+*/
+
+int main(void)
+{
+int i;
+printf("infinite incoming :(\n");
+i=0;
+while (i < 10) /*causes the output to go into an infinite loop*/
+{
+putchar(i);
+}
+printf("infinite loop avoided! \\o/n\n");
+return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+These were wrongly coded,then debugged.

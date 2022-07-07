cc4b091f80ffdb56375c2f482bb397ee1f8bee1d
Benson <benkabenson@gmail.com>
2022-07-07 08:23:51 -0700

    debug

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 
-while (i < 10)
-{
-putchar(i); /* causes the output to go into an infinite loop */
-}
+/*while (i < 10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
 
 printf("Infinite loop avoided! \\o/\n");
 

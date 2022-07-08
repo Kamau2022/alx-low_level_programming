e3489bda6a8c985abd77c94d634cb4857f46df6f
Benson <benkabenson@gmail.com>
2022-07-08 16:44:16 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
-/*more_numbers - a function that prints numbers
+/**
+*more_numbers - a function that prints numbers
 *followed by a new line.
-Return type: Always 0
+*Return type: Always 0
 */
 
 void more_numbers(void)
 {
 	int i = 0, j;
-	
+
 	while (i <= 9)
 	{
 		j = 0;
 	while (j <= 14)
 	{
-		_putchar(j/10 + '0');
-		_putchar(j%10 + '0');
+		_putchar(j / 10 + '0');
+		_putchar(j % 10 + '0');
 		++j;
 	}
 		_putchar('\n');

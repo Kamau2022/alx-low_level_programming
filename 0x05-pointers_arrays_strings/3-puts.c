87ec93374fb4013571356403c61a90258dcc2cd5
Benson <benkabenson@gmail.com>
2022-07-11 16:17:52 -0700

    print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*_puts - a function that prints a string
+*@str: a pointer to the string we want to print
+*Return: nothing
+*/
+
+void _puts(char *str)
+{
+char c = *str;
+
+_putchar(c);
+}

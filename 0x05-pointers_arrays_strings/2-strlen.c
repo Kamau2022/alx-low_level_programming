73458ec2f870d45a3e9eecbb0e646735ec827f23
Benson <benkabenson@gmail.com>
2022-07-11 15:16:45 -0700

    Display length of the string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h> 
+/**
+*_strlen - a function that gives the lenght of a string
+*@s - a character variable
+*Return: length of a string
+*/
+
+int _strlen(char *s)
+{
+	int length;
+	length = strlen(s);
+	return (length);
+}

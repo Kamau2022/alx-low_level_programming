0a11a1b64298da53b2f842dda7a286fd90757ec2
Benson <benkabenson@gmail.com>
2022-07-10 23:25:39 -0700

    checking for a digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -8,19 +8,15 @@
 
 int _isdigit(int c)
 {
-	int digit, k = 0;
-	char j = 32;
-	char m = 58;
+	int digit;
+	char k = '0';
+	
 
 	if (c >= k && c <= '9')
 	{
 		digit = 1;
 	}
-	else if ((c >= j && c <= 47) || (c >= m && c <= 126))
-	{
-		digit = 0;
-	}
-	else
+	else 
 	{
 		digit = 0;
 	}

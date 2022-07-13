4a5c0aa059819c3873f6142b426c4a729b0ffe6a
Benson <benkabenson@gmail.com>
2022-07-13 14:44:28 -0700

    concatenates strings using strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+#define DEST_SIZE 100
+
+/**
+*_strncat - a function that concatenate strings
+*@dest: a character input
+*@char: a character input
+*@n: an integer input
+*Return: a pointer
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *k = dest;
+	char *c = src;
+	int i = 0;
+
+	while (i == n)
+	{
+	i++;
+	}
+	{
+	strncat(k, c, n);
+	}
+	return (k);
+}
+

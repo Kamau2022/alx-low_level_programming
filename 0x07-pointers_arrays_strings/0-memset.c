964c74d4295416f150a96ae68ce6981ad63c44c7
Benson <benkabenson@gmail.com>
2022-07-18 10:24:05 -0700

    writing into memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+*_memset - a function that fills memory with 
+*a constant byte
+*@s: address of the memory to print
+*@b: value to be set
+*@n: size of memory to print
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+	_memset(s, 'b', n);
+	
+	return (s);
+}
+

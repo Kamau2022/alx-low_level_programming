5efba9c946524e651cb62cf978532a8e8dc604d4
Benson <benkabenson@gmail.com>
2022-07-19 07:26:54 -0700

    hot fix 6

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,10 +13,12 @@ char *_memset(char *s, char b, unsigned int n)
 {
 
 	unsigned int i = 0;
+	char *c = s;
 
 	for (i =0; i < n; ++i)
 	{
-		s[i] = b;
+		c[i] = b;
+		_memset (c, b, n);
 	}
-	return (s);
+	return (c);
 	}

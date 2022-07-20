477db2f2607b48745e9efa2691cf7a49453d9b93
Benson <benkabenson@gmail.com>
2022-07-20 04:25:07 -0700

    first fix

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_puts_recursion - a function that prints a string
+*followed by a new line
+*@s: an input string
+*Return: nothing
+*/
+
+void _puts_recursion(char *s)
+{
+	char *k = s;
+
+	puts(k);
+	}
+

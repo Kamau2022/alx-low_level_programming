d3408d2d9069af4be58dd612f435a8bdf8946398
Benson <benkabenson@gmail.com>
2022-07-22 06:41:17 -0700

    program's name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main - check main
+*@argc: number of arquments
+*@argv: an array of strings
+*Return: Always 0
+*/
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+

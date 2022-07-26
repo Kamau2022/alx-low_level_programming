43aceb8dc5e8440d2e6f503f2dc6556e461952c3
Benson <benkabenson@gmail.com>
2022-07-26 08:52:57 -0700

    fix

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*create_array - a function that creates an array of characters
+*@size: the size of an array
+*@c: an input character
+*Return: a character
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	i = 0;
+	char *k = &c;
+
+	while (i < size)
+	{
+	if (size == 0)
+	{
+	return (NULL);
+	}
+	if (*k != 0)
+	{
+	_putchar(k[i]);
+	++i;
+	}
+	if (*k == 0)
+	{
+	break;
+	}
+	return (k);
+	}
+	return (NULL);
+	}
+	

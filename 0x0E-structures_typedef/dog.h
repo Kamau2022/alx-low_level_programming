2225bc655be6fe514c3e09e2734ccfe14044875d
Benson <benkabenson@gmail.com>
2022-08-01 13:04:39 -0700

    fixed

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef struct
+#define struct
+#undef struct
+/**
+*struct dog - contain details of a dog
+*@name: name of the dog
+*@age: age of the dog
+*@owner: owner of the dog
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif

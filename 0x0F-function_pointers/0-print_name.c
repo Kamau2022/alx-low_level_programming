fef2beb884519bafe94673fc61b0321db3b88a19
Benson <benkabenson@gmail.com>
2022-08-04 07:01:52 -0700

    fixed 4

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,15 @@
 #include "function_pointers.h"
 #include <string.h>
+/**
+*print_name - a function that prints a name
+*@name: name to print
+*@f: function to send the name to
+*Return: nothing
+*/
 
 void print_name(char *name, void(*f)(char *))
 {
+if (name == NULL || f == NULL)
+return;
 (*f) (name);
 }

2a3bd3249d4ef7b3bf165ead8d9ba6ee33a6b48e
Benson <benkabenson@gmail.com>
2022-08-15 14:00:55 -0700

    fix

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,18 +4,39 @@
 
 /**
 *_strlen - returns the length of a string
-*@s: string
+*@str: string
 *Return: length of the string
+*/
+unsigned int _strlen(char *str)
+{
+unsigned int i;
+for(i = 0; str[i]; i++)
+;
+return(i);
+}
+/**
+*add_note - adds a node at the beginning
+*@head: pointer to a list
+*@str: string to add to new node
+*Return: pointer to the new node
 */
 
 list_t *add_note(list_t **head, const char *str)
 {
 list_t *new;
+if (str == NULL)
+return (NULL);
 new = malloc(sizeof(list_t));
 if (new == NULL)
 return (NULL);
 new->str = strdup(str);
+if (new->str == NULL)
+{
+free(new);
+return (NULL);
+new->len = _strlen(new->str);
 new->next = *head;
 *head = new;
+}
 return (new);
 }

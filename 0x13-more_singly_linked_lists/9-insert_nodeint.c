70c2d443e9d8297c666069ed4e6907d11ed56f59
Benson <benkabenson@gmail.com>
2022-08-17 17:05:03 -0700

    hotfix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,24 +2,6 @@
 #include <stdlib.h>
 
 /**
-*find_length - finds length of a linked list
-*@head: head node
-*Return: number of elements
-*/
-
-int find_length(listint_t **head)
-{
-size_t count;
-count = 0;
-while (head != NULL)
-{
-*head = (*head)->next;
-count++;
-}
-return (count);
-}
-
-/**
 *insert_nodeint_at_index - inserts a new node at a given position
 *@head: head node
 *@idx: index of the list to add the new node
@@ -32,38 +14,37 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 listint_t *newnode;
 listint_t *temp;
 unsigned int i = 0;
-
+newnode = malloc(sizeof(listint_t));
 if (head == NULL)
 {
 return (NULL);
 }
-else
+if (newnode == NULL)
 {
-temp = *head;
+return (NULL);
+}
+if (idx == 0)
+{
+newnode->next = *head;
+newnode->n = n;
+*head = newnode;
+return (newnode);
+}
+if (*head == NULL)
+{
+*head = newnode;
+newnode->n = n;
+newnode->next = NULL;
+return (newnode);
+}
 while (i < idx)
 {
+temp = *head;
 temp = temp->next;
 i++;
 }
-newnode = malloc(sizeof(listint_t));
 newnode->n = n;
 newnode->next = temp->next;
 temp->next = newnode;
-}
 return (newnode);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

bddbe4a08c788115365e18e439ff5f416893abc8
Benson <benkabenson@gmail.com>
2022-08-19 14:50:44 -0700

    error fixed

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,9 +16,11 @@ int i = 0;
 unsigned int len;
 len = strlen(b);
 
+if (b == NULL)
+return (0);
 for (i = len - 1; i >= 0; i--)
 {
-if ((b[i] != '0' && b[i] != '1') || (b == NULL))
+if (b[i] != '0' && b[i] != '1')
 	return (0);
 
 if (b[i] == '1')

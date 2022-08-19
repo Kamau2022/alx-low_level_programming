1288cdcbbb5726039821870ccd0f16c2f19ae9f1
Benson <benkabenson@gmail.com>
2022-08-19 14:32:44 -0700

    error fixed

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <string.h>
+#include <stdlib.h>
 
 /**
 *binary_to_uint - converts binary number to a unsigned int
@@ -13,14 +14,13 @@ unsigned int k = 0;
 unsigned int j = 1;
 int i = 0;
 unsigned int len;
-
 len = strlen(b);
 
 for (i = len - 1; i >= 0; i--)
 {
 if (b[i] != '0' && b[i] != '1')
 	return (0);
-if (b[i] == '\040')
+if (b == NULL)
 	return (0);
 
 if (b[i] == '1')

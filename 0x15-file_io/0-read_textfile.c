7b4d21b55ed093c3f7d0c041c70d2e1d7a2995cb
Benson <benkabenson@gmail.com>
2022-08-23 11:41:04 -0700

    hot fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,10 +3,18 @@
 #include <sys/stat.h>
 #include<fcntl.h>
 
+/**
+*read_textfile - a function that reads a text file and prints
+*it to the POSIX standard output
+*@filename: the file to read from
+*@letters: numbers of letters it should read and print
+*Return: 0
+*/
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 int fd;
-char buffer[1024];
+char buffer[2024];
 fd = open("filename", O_RDONLY);
 if (filename == NULL)
 	return (0);

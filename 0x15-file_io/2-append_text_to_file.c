4a9c32144d08d494ca3070394a0c5ada068fa271
Benson <benkabenson@gmail.com>
2022-08-25 13:17:05 -0700

    fixed

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,9 +18,19 @@ int append_text_to_file(const char *filename, const char *text_content)
 int fd;
 int i, kz;
 
-fd = open(filename, O_WRONLY | O_APPEND);
-if (filename == NULL || text_content == NULL)
+if (filename == NULL)
+{
 	return (-1);
+}
+if (text_content == NULL)
+{
+	return (1);
+}
+fd = open(filename, O_WRONLY | O_APPEND);
+if (fd == -1)
+{
+	return (-1);
+}
 for (i = 0; text_content[i]; i++)
 ;
 kz = write(fd, text_content, i);

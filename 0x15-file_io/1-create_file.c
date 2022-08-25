754e24a947d7b848741cf004cb940ceceeff4b27
Benson <benkabenson@gmail.com>
2022-08-25 13:23:11 -0700

    fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,35 +15,24 @@
 int create_file(const char *filename, char *text_content)
 {
 int fd;
-int i, sz, kz;
+int i, sz;
 if (filename == NULL)
 	return (-1);
-fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+fd = open(filename ,O_CREAT | O_TRUNC | O_WRONLY, 0600);
 if (fd == -1)
 {
 	return (-1);
-	return (1);
 }
 if (text_content == NULL)
 {
-kz = open(filename, O_WRONLY | O_CREAT, 0600);
-}
-if (kz == -1)
-{
-	return (-1);
-}
-else if (kz != -1)
-{
+close(fd);
 return (1);
 }
 for (i = 0; text_content[i]; i++)
-{
+;
 sz = write(fd, text_content, i);
 close(fd);
-}
 if (sz == -1)
-{
-	return (-1);
-}
+return (-1);
 return (1);
 }

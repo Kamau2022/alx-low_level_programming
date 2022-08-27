288178dfd4e10a1218dd6ba8356def869ab40b10
Benson <benkabenson@gmail.com>
2022-08-27 08:47:57 -0700

    hot fix

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*mul - function multiplies two integers
+*@a: first integer input
+*@b: second integer input
+*Return: Always 0
+*/
+
+int mul(int a, int b)
+{
+int product;
+product = a * b;
+return (product);
+}

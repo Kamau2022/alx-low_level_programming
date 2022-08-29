4c4382c122ebfcd1f995c03e9e4d28c42ebe4d59
Benson <benkabenson@gmail.com>
2022-08-29 00:27:52 -0700

    an if statement was added

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,14 +13,15 @@ void print_square(int size)
 char c = '#';
 int i = 0;
 int j = 0;
-
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
 for (i = 0; i < size; i++)
 {
 for (j = 0; j < size; j++)
 {
-if (size <= 0)
-_putchar('\n');
-else
 _putchar(c);
 }
 _putchar('\n');

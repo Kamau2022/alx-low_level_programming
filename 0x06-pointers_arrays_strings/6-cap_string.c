65c89f8e2059d4dd96dfc9aa1c7f05cbc1e56be0
Benson <benkabenson@gmail.com>
2022-09-13 05:20:44 -0700

    updated

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
 *cap_string - capitalizes all words of a string
@@ -13,12 +12,10 @@ int i;
 
 for (i = 0; str[i] != '\0'; i++)
 {
-{
-if ((str[i] == 0 && (str[i] >= 'a' && str[i] <= 'z')))
+if (i == 0 &&  (str[i] >= 'a' && str[i] <= 'z'))
 {
 str[i] = str[i] - 32;
 continue;
-}
 }
 while ((str[i] == ' ' || str[i] == '\n' || str[i] == '.' || str[i] == '\t'))
 {

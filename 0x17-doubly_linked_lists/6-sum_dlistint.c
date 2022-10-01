e52a56f3958ec29f8a6842112b722497cb70d888
kamau2022 <benkabenson@gmail.com>
2022-10-01 13:11:44 -0700

    first commit

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 
 /**
-*sum_dlistint - a function that returns the sum 
+*sum_dlistint - a function that returns the sum
 *of all the data (n) of a dlistint_t linked list.
 *@head: a head to a doubly linked list
-*Return: nothing
+*Return: sum or 0
 */
 
 int sum_dlistint(dlistint_t *head)
 {
 int sum = 0;
-int k;
 dlistint_t *temp;
+
 temp = head;
 
 if (head == NULL)
@@ -20,8 +20,7 @@ return (0);
 }
 while (temp != NULL)
 {
-k = temp->next;
-sum = sum + k;
+sum = sum + temp->n;
 temp = temp->next;
 }
 return (sum);

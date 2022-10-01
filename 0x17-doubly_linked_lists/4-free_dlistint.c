203a5ccfc83805946f7e2b55dd9931ffa55aec8b
kamau2022 <benkabenson@gmail.com>
2022-10-01 13:05:14 -0700

    first commit

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+*free_dlistint - a function that frees a dlistint_t list.
+*@head: a pointer to a doubly linked list
+*
+*Return: nothing
+*/
+
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *temp;
+while (head != NULL)
+{
+temp = head;
+head = head->next;
+free(temp);
+}
+}

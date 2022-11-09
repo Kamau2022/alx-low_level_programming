15bb9482c6febdd8c702ca1a37d96374740744c8
kamau2022 <benkabenson@gmail.com>
2022-11-09 15:29:55 +0000

    additions

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,8 @@ if (d->name == NULL)
 {
 printf("Name: (nil)\n");
 }
-printf("Name: %s\n", d->name);
+}
+/*printf("Name: %s\n", d->name);
 printf("Age: %f\n", d->age);
 printf("Owner: %s\n", d->owner);
-}
+}*/
